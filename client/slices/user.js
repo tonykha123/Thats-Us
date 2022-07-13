e0c577d1bbfb1fbb551396490a80f7fa915d8327
rakim-up <103359076+rakim-up@users.noreply.github.com>
2022-07-14 09:34:11 +1200

    Initial commit

---
+++ client/slices/user.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const emptyUser = {
+  auth0Id: '',
+  email: '',
+  token: '',
+}
+
+const slice = createSlice({
+  name: 'user',
+  initialState: emptyUser,
+  reducers: {
+    setUser: (_, { payload }) => payload,
+    clearUser: () => emptyUser,
+  },
+})
+
+export const { setUser, clearUser } = slice.actions
+export default slice.reducer
