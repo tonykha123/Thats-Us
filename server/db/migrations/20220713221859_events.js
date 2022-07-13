578ba6ae8d2ca06dfd1d8be7c90d49edc3404b1e
rakim-up <ruraleproctor@gmail.com>
2022-07-14 11:04:20 +1200

    migrations and seeds set for events table

---
+++ server/db/migrations/20220713221859_events.js
@@ -0,0 +1,16 @@
+exports.up = (knex) => {
+  return knex.schema.createTable('events', (table) => {
+    table.increments('event_id')
+    table.string('name')
+    table.string('status')
+    table.string('date')
+    table.string('time')
+    table.integer('max')
+    table.string('IMG')
+    table.string('description')
+  })
+}
+
+exports.down = (knex) => {
+  return knex.schema.dropTable('events')
+}
