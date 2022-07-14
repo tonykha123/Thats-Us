4d87e2e778cbb96cb056c1d705564e10dfccb1b7
saia-maka <makasiosaia@gmail.com>
2022-07-14 22:19:12 +1200

    fetch upcoming events and render in upcomingEvents.jsx

--- client/components/EventsComponents/UpcomingEvents/UpcomingEvents.jsx
+++ client/components/EventsComponents/UpcomingEvents/UpcomingEvents.jsx
@@ -1,46 +1,29 @@
-import React from 'react'
-
-// COMPONENT IMPORTS:
+import React, { useState, useEffect } from 'react'
 import { SingleUpcomingTile } from './SingleUpcomingTile'
+import { getAllEvents } from '../../apiFuncs/eventApi'
 
 const UpcomingEvents = () => {
-  const testData = [
-    {
-      event_id: 1,
-      name: 'Tag',
-      status: 'Live',
-      date: '14/7/22',
-      max: 12,
-      description:
-        'Tup for chill game of tag. Bring your own boots - water & food provided',
-    },
-    {
-      event_id: 2,
-      name: '3v3 B-ball',
-      status: 'Up-coming',
-      date: '14/7/22',
-      max: 6,
-      description: 'Keen for 3v3 game lets go',
-    },
-    {
-      event_id: 3,
-      name: 'Volley ball',
-      status: 'Up-coming',
-      date: '14/7/22',
-      max: 12,
-      description:
-        'Looking for another team to join our tournament, beaches, balls & bikinis',
-    },
-  ]
+  const [events, setEvents] = useState([])
+
+  useEffect(async () => {
+    const all = await getAllEvents()
+    const evts = all.filter((evt) => evt.status == 'upcoming')
+    try {
+      setEvents(evts)
+    } catch {
+      console.error('failed to fetch all upcoming events')
+    }
+  }, [])
+
   return (
     <div>
-      {testData.map((data) => (
+      {events.map(({ event_id, name, date, max, description }) => (
         <SingleUpcomingTile
-          key={data.event_id}
-          name={data.name}
-          date={data.date}
-          max={data.max}
-          description={data.description}
+          key={event_id}
+          name={name}
+          date={date}
+          max={max}
+          description={description}
         />
       ))}
     </div>
