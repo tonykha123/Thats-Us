ce594bbf48243866966b185fe8f99a798042fb74
Tony K <tonykha.1705@gmail.com>
2022-07-14 21:44:02 +1200

    second type animation

--- client/components/Hero/Hero.jsx
+++ client/components/Hero/Hero.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import Typed from 'react-typed'
+import TypeAnimation from 'react-type-animation'
 
 const Hero = () => {
   return (
-    <section className="w-full h-[90vh] flex flex-col p-4 sm:flex-row sm:w-10/12 my-5 sm:px-10 s:pt-6 sm:justify-center">
+    <section className="w-full h-[90vh] flex flex-col p-4 mx-auto sm:flex-row sm:w-10/12 my-5 sm:px-10 s:pt-6 sm:justify-center">
       <div className=" w-2/3 mr-5 sm:w-6/12 self-center text-center sm:text-left">
         {/*text side */}
         <Typed
           strings={['Ball?', 'Drinkups?', 'Crew?']}
-          typeSpeed={170}
-          backSpeed={190}
+          typeSpeed={300}
+          backSpeed={300}
           loop
+          className="text-4xl w-full font-extrabold sm:text-6xl "
+        />
+        <TypeAnimation
+          cursor={true}
+          sequence={['Ball?', 3500, 'Drinkups?', 3500, 'Crew?', 3500]}
+          wrapper="p"
+          repeat={Infinity}
           className="text-4xl w-full font-extrabold sm:text-6xl "
         />
         <p className="text-4xl w-full font-extrabold sm:text-6xl ">Thats us.</p>
@@ -23,7 +31,7 @@ const Hero = () => {
       </div>
       <div className="self-center">
         <img
-          className="shadow-xl object-fill my-5"
+          className="shadow-xl object-fill my-5 mx-auto"
           src="/images/Friendly.jpeg"
           alt="Group of friendly people laughing"
         />
