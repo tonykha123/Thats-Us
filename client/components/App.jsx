64aa0959e876f33178b56e7f483445f054a4d171
saia-maka <makasiosaia@gmail.com>
2022-07-16 10:56:32 +1200

    rename app route path for events & navigate to event details from liveEvents

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/description" element={<Details />} />
+            <Route path="/event/:id" element={<Details />} />
             <Route path="/add" element={<AddEvent />} />
           </Routes>
         </Router>
