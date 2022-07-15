01df2840f1aaa6f89ec9318584e343055bd5de1b
saia-maka <makasiosaia@gmail.com>
2022-07-16 11:40:35 +1200

    tidy and refactor backend

--- server/server.js
+++ server/server.js
@@ -2,8 +2,6 @@ const path = require('path')
 const express = require('express')
 
 const eventsRoutes = require('./routes/events')
-const eventDetails = require('./routes/eventDetails')
-const addEvent = require('./routes/addEvent')
 const users = require('./routes/users')
 
 const server = express()
@@ -12,8 +10,6 @@ server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
 server.use('/api/v1/events', eventsRoutes)
-server.use('/api/v1/details', eventDetails)
-server.use('/api/v1/add', addEvent)
 server.use('/api/v1/users', users)
 
 
