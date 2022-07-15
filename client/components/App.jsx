c8e2b6c6e7d12b695fe8faa04a0b2863a71e6824
saia-maka <makasiosaia@gmail.com>
2022-07-15 16:14:00 +1200

    prototype auth0-setup

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// AUTH0 IMPORTS:
+import { cacheUser } from '../auth0-utils'
+
+import { useAuth0 } from '@auth0/auth0-react'
 
 // COMPONENT IMPORTS
 import NavBar from './Nav/NavBar'
+import Register from './Register/Register'
 import Home from './Home/Home'
 import Details from './Details/Details'
 
 const App = () => {
+  cacheUser(useAuth0)
   return (
     <>
       {/* outer most container */}
@@ -15,6 +21,7 @@ const App = () => {
           <NavBar />
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
             <Route path="/description" element={<Details />} />
           </Routes>
         </Router>
