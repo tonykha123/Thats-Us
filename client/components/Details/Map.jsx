8f7cd7bd556a1689c4913d085a1627086419575e
rakim-up <ruraleproctor@gmail.com>
2022-07-15 14:50:02 +1200

    map working! YESSIR

--- client/components/Details/Map.jsx
+++ client/components/Details/Map.jsx
@@ -1,37 +1,19 @@
 import React from "react";
-  import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
+  import {MapContainer, TileLayer, Marker, Popup, useMap} from 'react-leaflet'
 
-export default function Map(){
+const Map = () => {
   return(
-    // <MapContainer className="h max">
-    // </MapContainer>
-
-
-<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-  <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-  <Marker position={[51.505, -0.09]}>
-    <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
-  </Marker>
-</MapContainer>
-
+    <MapContainer center={[-36.86667, 174.76667]}
+     zoom={13} 
+     scrollWheelZoom={false}
+     style={{width: '40%', height: '300px', marginLeft: 'auto'}}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    </MapContainer> 
   )
 }
 
-{/* <MapContainer 
-className="h-full w-full" 
-center={[-36.86441, 174.77631]}
-zoom={12}
-scrollWheelZoom ={true}
->
-  <TileLayer
-   attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-   >
-  </TileLayer>
-
-</MapContainer> */}
+export default Map
+ 
