a7e3e43b29192cb624f7f4fd4067167909972d68
Tony K <tonykha.1705@gmail.com>
2022-07-16 17:22:25 +1200

    mobile footer done

--- client/components/Footer/Footer.jsx
+++ client/components/Footer/Footer.jsx
@@ -9,36 +9,42 @@ import {
 const Footer = () => {
   return (
     // main footer container
-    <footer className="flex flex-col items-center">
-      {/* icons left side*/}
-      <div className="mt-6">
-        <p>Follow Us</p>
-        <div className="flex justfy-between">
-          <FaFacebookSquare size={30} className="text-red-500" />
-          <FaInstagram size={30} />
-          <FaTwitterSquare size={30} />
-          <FaGithubSquare size={30} />
+    <footer className="w-full bg-gray-600 text-gray-300">
+      {/* Everything container */}
+      <div className="w-10/12 flex flex-col items-center mx-auto ">
+        {/* icons and p tag container*/}
+        <div className="mt-6 w-10/12 flex flex-col items-center">
+          {/* logo goes here */}
+          <p className="mb-4 text-lg font-medium">Follow Us</p>
+          {/* icons container */}
+          <div className="flex justify-between w-full">
+            <FaFacebookSquare size={30} className="" />
+            <FaInstagram size={30} />
+            <FaTwitterSquare size={30} />
+            <FaGithubSquare size={30} />
+          </div>
+        </div>
+        {/* About */}
+        <div className="mt-6 w-10/12 text-align">
+          <h6 className="text-lg font-medium text-gray-200"> That's Us</h6>
+          <ul className="">
+            <li className="hover:text-gray-400 hover:underline">About</li>
+            <li className="hover:text-gray-400 hover:underline">The Team</li>
+            <li className="hover:text-gray-400 hover:underline">Blog</li>
+            <li className="hover:text-gray-400 hover:underline">Careers</li>
+          </ul>
+        </div>
+        {/* discover */}
+        <div className="my-6 w-10/12">
+          <h6 className="text-lg font-medium text-gray-200"> Discover</h6>
+          <ul>
+            <li className="hover:text-gray-400 hover:underline">Groups</li>
+            <li className="hover:text-gray-400 hover:underline">Topic</li>
+            <li className="hover:text-gray-400 hover:underline">Cities</li>
+          </ul>
         </div>
       </div>
-      {/* About */}
-      <div className="mt-6">
-        <h6> That's Us</h6>
-        <ul>
-          <li>About</li>
-          <li>The Team</li>
-          <li>Blog</li>
-          <li>Careers</li>
-        </ul>
-      </div>
-      {/* discover */}
-      <div className="my-6">
-        <h6> Discover</h6>
-        <ul>
-          <li>Groups</li>
-          <li>Topic</li>
-          <li>Cities</li>
-        </ul>
-      </div>
+      <p className="text-md">© 2022 That's Us</p>
     </footer>
   )
 }
