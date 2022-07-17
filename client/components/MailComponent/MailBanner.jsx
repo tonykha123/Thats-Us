2748e4372845a965f1dbc49cd7fbbdd0df5130ea
Tony K <tonykha.1705@gmail.com>
2022-07-17 13:18:02 +1200

    banner responsive

--- client/components/MailComponent/MailBanner.jsx
+++ client/components/MailComponent/MailBanner.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { FaHandshake } from 'react-icons/fa'
+import { BsFillCalendarDayFill } from 'react-icons/bs'
+import { IoIosPeople } from 'react-icons/io'
 
 const MailBanner = () => {
   return (
-    <section className="w-full h-[65vh] flex flex-col items-center bg-white">
-      <div className="w-full flex flex-col items-center my-4">
+    <section className="w-full h-[88vh]  lg:h-[65vh] flex flex-col items-center bg-white">
+      <div className="w-full flex flex-col items-center my-4 lg:my-10">
         <h1 className="text-3xl font-bold mx-auto mb-2">How it works</h1>
         <p className="text-center mx-auto w-9/12">
           Get to know new people who share your interestes through online and
@@ -13,6 +16,7 @@ const MailBanner = () => {
 
       <div className="flex flex-col lg:flex-row">
         <div className="w-full flex flex-col items-center my-4">
+          <FaHandshake size={65} />
           <h2 className="text-2xl font-bold mx-auto mb-2">Find an event</h2>
           <p className="text-center mx-auto w-9/12">
             Looking to explore what's going on in your community? Checkout your
@@ -21,12 +25,14 @@ const MailBanner = () => {
           </p>
         </div>
         <div className="w-full flex flex-col items-center my-4">
+          <BsFillCalendarDayFill size={55} />
           <h1 className="text-2xl font-bold mx-auto mb-2">Create an event</h1>
           <p className="text-center mx-auto w-9/12">
             Make an event and have it be visible to other members! It's easy as!
           </p>
         </div>
         <div className="w-full flex flex-col items-center my-4">
+          <IoIosPeople size={65} />
           <h1 className="text-2xl font-bold mx-auto mb-2">
             Look at upcoming events
           </h1>
