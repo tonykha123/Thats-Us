690cea1b1e039960d81303fc9f302c6ae4672d4e
Tony K <tonykha.1705@gmail.com>
2022-07-20 21:15:01 +1200

    done landing

--- client/components/MailComponent/MailBanner.jsx
+++ client/components/MailComponent/MailBanner.jsx
@@ -7,7 +7,9 @@ const MailBanner = () => {
   return (
     <section className="w-full h-[100vh]  lg:h-[65vh] flex flex-col items-center bg-white">
       <div className="w-full flex flex-col items-center my-4 lg:my-10">
-        <h1 className="text-3xl font-bold mx-auto mb-2">How it works</h1>
+        <h1 className="text-3xl font-bold mx-auto mb-2 text-[#00AEFF]">
+          How it works
+        </h1>
         <p className="text-center mx-auto w-9/12">
           Get to know new people who share your interests through online and
           in-person events.
@@ -16,8 +18,10 @@ const MailBanner = () => {
 
       <div className="flex flex-col lg:flex-row">
         <div className="w-full flex flex-col items-center my-4">
-          <FaHandshake size={65} />
-          <h2 className="text-2xl font-bold mx-auto mb-2">Find an event</h2>
+          <FaHandshake size={65} className="text-[#FFDC00]" />
+          <h2 className="text-2xl font-bold mx-auto mb-2  text-[#00AEFF]">
+            Find an event
+          </h2>
           <p className="text-center mx-auto w-9/12">
             Looking to explore what's going on in your community? Checkout your
             local events and connect with friendly members! There's always
@@ -25,15 +29,17 @@ const MailBanner = () => {
           </p>
         </div>
         <div className="w-full flex flex-col items-center my-4">
-          <BsFillCalendarDayFill size={55} className="mb-2" />
-          <h1 className="text-2xl font-bold mx-auto my-2">Create an event</h1>
+          <BsFillCalendarDayFill size={55} className="mb-2 text-[#FFDC00]" />
+          <h1 className="text-2xl font-bold mx-auto my-2 text-[#00AEFF]">
+            Create an event
+          </h1>
           <p className="text-center mx-auto w-9/12">
             Make an event and have it be visible to other members! It's easy as!
           </p>
         </div>
         <div className="w-full flex flex-col items-center my-4">
-          <IoIosPeople size={65} />
-          <h1 className="text-2xl font-bold mx-auto mb-2">
+          <IoIosPeople size={65} className="mb-2 text-[#FFDC00]" />
+          <h1 className="text-2xl font-bold mx-auto mb-2 text-[#00AEFF]">
             Look at upcoming events
           </h1>
           <p className="text-center mx-auto w-9/12">
