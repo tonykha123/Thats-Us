46cd1c1fe3d48dbd7837d9ead33d1f991422d35c
Tony K <tonykha.1705@gmail.com>
2022-07-27 15:50:05 +1200

    removed further components

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 
 const eventsRoutes = require('./routes/events')
 const users = require('./routes/users')
-// const api = require('./routes/api')
+
 const server = express()
 
 server.use(express.json())
@@ -11,7 +11,6 @@ server.use(express.static(path.join(__dirname, 'public')))
 
 server.use('/api/v1/events', eventsRoutes)
 server.use('/api/v1/users', users)
-// server.use('/api/v1/users', api)
 
 server.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'))
